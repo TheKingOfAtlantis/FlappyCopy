5a81dd31a05b5ec39dd0f356bf5c92762972b8cd
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-10-03 16:25:18 +0100

    Scrolling Skybox
    Animated the skybox to have clouds which scroll across

---
+++ Assets/Scripts/SkyboxScroll.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxScroll : MonoBehaviour
+{
+
+    public float velocity = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * velocity;
+        if(transform.position.x <= -7)
+        {
+            var pos = transform.position;
+            pos.x = 0;
+            transform.position = pos;
+        }
+    }
+}
