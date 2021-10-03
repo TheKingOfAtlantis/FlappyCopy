06e872829a8f59481024686a05f0ae517298e496
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-10-04 00:57:19 +0100

    Game Controller
    A singleton containing various objects that I need only one of

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+public class GameController : MonoBehaviour
+{
+
+    static public GameController Instance;
+    // Use this for initialization
+
+    void Awake()
+    {
+        // If the game controller doesn't exist yet create it
+        // If it does and this isn't the same as the instance then destroy this 
+        if (Instance == null) Instance = this;
+        else if (Instance != this) Destroy(gameObject);
+    }
+
+    void Start()
+    {
+    }
+}
