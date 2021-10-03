3d82adb945b6100d2da584b80ea24258deb57b6c
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-10-03 13:38:52 +0100

    Bird Animations
    Setup the necessary logic for the bird to flap and die

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float forceMagnitude; // Magnitude of the force to be applied to the bird
+    
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator  = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Animate the bird as long as it's not dead
+        if (isDead == false)
+        {
+            // If we are given an input
+            if (Input.GetMouseButton(0) || Input.GetKeyDown("space"))
+            {
+                // Make the bird move upwards
+                rigidBody.velocity = Vector2.zero;
+                rigidBody.AddForce(Vector2.up * forceMagnitude);
+                // And aniamte it flapping
+                animator.SetTrigger("Flap");
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If we hit something
+        // Kill the bird :(
+        animator.SetTrigger("Die");
+        isDead = true;
+    }
+}
